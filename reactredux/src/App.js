16cb3f4715dd8b7efde7c70c662a8020179d04ad
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-06-30 14:22:08 -0300

    Primeira versão do exemplo

--- reactredux/src/App.js
+++ reactredux/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Sidebar from "./components/Sidebar";
+import Video from "./components/Video";
 
 function App() {
-  return <div className="App">teste</div>;
+  return (
+    <div className="App">
+      <Video />
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
