eca7f744c8a0a72b4e4d69c86e4fdb24598ef573
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-06-30 14:22:23 -0300

    Segunda versão do exemplo

--- reactredux/src/App.js
+++ reactredux/src/App.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import Sidebar from "./components/Sidebar";
 import Video from "./components/Video";
 
-function App() {
-  return (
-    <div className="App">
-      <Video />
-      <Sidebar />
-    </div>
-  );
-}
+export default class App extends Component {
+  state = {
+    activeLesson: {},
+    modules: [
+      {
+        id: 1,
+        title: "Iniciando com React",
+        lessons: [
+          { id: 1, title: "Primeira aula" },
+          { id: 2, title: "Segunda aula" },
+        ],
+      },
+      {
+        id: 2,
+        title: "Aprendendo Redux",
+        lessons: [
+          { id: 3, title: "Terceira aula" },
+          { id: 4, title: "Quarta aula" },
+        ],
+      },
+    ],
+  };
 
-export default App;
+  render() {
+    return (
+      <div className="App">
+        <Video />
+        <Sidebar modules={this.state.modules} />
+      </div>
+    );
+  }
+}
