ea09453352142e024d1320b295df4000d1a9e99d
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-06-30 14:35:38 -0300

    Cria o estado global

---
+++ reactredux/src/store/index.js
@@ -0,0 +1,27 @@
+import { createStore } from "redux";
+// createStore: cria o estado global
+
+function reducer() {
+  return [
+    {
+      id: 1,
+      title: "Iniciando com React",
+      lessons: [
+        { id: 1, title: "Primeira aula" },
+        { id: 2, title: "Segunda aula" },
+      ],
+    },
+    {
+      id: 2,
+      title: "Aprendendo Redux",
+      lessons: [
+        { id: 3, title: "Terceira aula" },
+        { id: 4, title: "Quarta aula" },
+      ],
+    },
+  ];
+}
+
+const store = createStore(reducer);
+
+export default store;
